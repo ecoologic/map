7e8189e84c5673391484e01240733160ee417fd8
Erik Ecoologic <erikecoologic@gmail.com>
2020-05-26 21:52:35 +1000

    Flag with useFetch

--- src/App.js
+++ src/App.js
@@ -7,38 +7,66 @@ import {
 } from './map/Layer'
 import {ViewCenter, ViewProvider} from './map/View';
 import {
-    ClickRecordProvider, Records,
+    ClickRecordProvider, ClickRecordContext, Records,
 } from './map/ClickRecord';
 import {MousePositionContext, MousePositionProvider} from './map/MousePosition';
 import {HoveredCountry, HoverProvider} from "./map/Hover";
 import {ActivatableContext} from "./utils";
 
+const useFetch = (url, options) => {
+    const fetchData = async () => {
+        try {
+            const rawResponse = await fetch(url, options);
+            const responseJson = await rawResponse.json();
+            setResponse(responseJson);
+        } catch (error) {
+            console.warn('fetch error', error);
+            setResponse({ error });
+        }
+    };
+    const [response, setResponse] = React.useState({});
+    React.useEffect(() => { fetchData() }, [url, options]);
+    return response;
+};
+
+const Flag = () => {
+    const {records} = React.useContext(ClickRecordContext);
+    const record = records[records.length - 1] || { featuresData: [{}] };
+    const country = record.featuresData[0];
+    const response = useFetch(`https://restcountries.eu/rest/v2/name/${country.name}?fullText=true`)
+    if(response[0]) {
+        return <img src={response[0]?.flag} alt={`${country.name} flag`} height="100px" />
+    } else {
+        return <span className="h-100px">{response.error || response.message}</span>
+    }
+}
+
 const App = () => {
     return <>
         <header className="header">
-            ===
+            A Basic Map By Ecoologic
         </header>
         <MapProvider>
             <ViewProvider>
                 <ViewCenter />
             </ViewProvider>
 
-            <ActivatableContext Provider={LayerProvider} Context={CountriesContext}/>
-            <ActivatableContext Provider={LayerProvider} Context={CitiesContext}/>
-            <ActivatableContext Provider={MousePositionProvider} Context={MousePositionContext}/>
+            <ActivatableContext Provider={LayerProvider} Context={CountriesContext} />
+            <ActivatableContext Provider={LayerProvider} Context={CitiesContext} />
+            <ActivatableContext Provider={MousePositionProvider} Context={MousePositionContext} />
             <HoverProvider>
                 <HoveredCountry />
                 <ClickRecordProvider>
-                    <Records/>
+                    <Flag />
+                    <Records />
                 </ClickRecordProvider>
             </HoverProvider>
 
         </MapProvider>
         <footer className="footer">
-            ---
+            Happy coding!
         </footer>
     </>
 };
 
 export default App
-
